2560a804654da9cfe0f677327c34ba6220c11e1c
korandr <korandrmail@ya.ru>
2026-06-07 18:10:02 +0300

    feat(lab2): добавлены графики нормированного амплитудного спектра в 1 задании

--- src/lab2_1.py
+++ src/lab2_1.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-n = np.arange(0, 128)      # Массив отсчётов
+N = 128                    # Количество отсчётов
+n = np.arange(0, N)        # Массив отсчётов
 T1 = 4                     # Период ВЧ синусоиды, отсчётов
 T2 = 32                    # Период НЧ синусоиды, отсчётов
 A1 = 0.5                   # Амплитуда ВЧ синусоиды, В
@@ -18,6 +19,8 @@ def build_plots(constant_component: bool) -> None:
 
     S = np.fft.fft(y)      # ДПФ
     A = np.abs(S)          # Амплитудный спектр
+    A_norm = A / (N / 2)   # Нормированный амплитудный спектр
+    A_norm[0] = A[0] / N
     F = np.angle(S)        # Фазовый спектр
     F_cleaned = np.where(A > 1e-10, F, 0) # Очищенный фазовый спектр
 
@@ -29,12 +32,21 @@ def build_plots(constant_component: bool) -> None:
     plt.grid(True)
     fig1.tight_layout()
 
-    fig2 = plt.figure(figsize=(9, 3.5))
-    plt.stem(n, A, linefmt='g-', markerfmt='go', basefmt='r-')
-    plt.title('Амплитудный спектр', fontsize=11, fontweight='bold')
-    plt.xlabel('Номер спектрального отсчета (k)')
-    plt.ylabel('Амплитуда спектра')
-    plt.grid(True)
+    fig2 = plt.figure(figsize=(9, 7))
+    ax1 = fig2.add_subplot(2, 1, 1)
+    ax1.stem(n, A, linefmt='g-', markerfmt='go', basefmt='r-')
+    ax1.set_title('Амплитудный спектр', fontsize=11, fontweight='bold')
+    ax1.set_xlabel('Номер спектрального отсчета (k)')
+    ax1.set_ylabel('Амплитуда спектра')
+    ax1.grid(True)
+
+    ax2 = fig2.add_subplot(2, 1, 2)
+    ax2.stem(n, A_norm, linefmt='g-', markerfmt='go', basefmt='r-')
+    ax2.set_title('Нормированный амплитудный спектр', fontsize=11, fontweight='bold')
+    ax2.set_xlabel('Номер спектрального отсчета (k)')
+    ax2.set_ylabel('Амплитуда, В')
+    ax2.grid(True)
+
     fig2.tight_layout()
 
     fig3 = plt.figure(figsize=(9, 7))
